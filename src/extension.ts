1081050ad4a4772854302d3317ea2b80cf996dfc
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-03-14 22:52:37 -0300

    Refactor how proxy is created passing context

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,13 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
-import proxy, { setContext } from "./proxy";
+import * as proxy from "./proxy";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(ctx: vscode.ExtensionContext) {
   // try to setup proxy
-  proxy.listen(3001, () => {
-    setContext(context);
-  });
+  proxy.setup(ctx, 3001, () => console.log("proxy is ready"));
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
@@ -27,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
     const fileName = vscode.window.activeTextEditor?.document.fileName;
     if (fileName) {
       const webviewContent = fs
-        .readFileSync(context.asAbsolutePath("public/webview.html"), "utf-8")
+        .readFileSync(ctx.asAbsolutePath("public/webview.html"), "utf-8")
         .replace("${fileName}", path.basename(fileName));
 
       panel.webview.html = webviewContent;
@@ -37,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(disposable);
+  ctx.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
